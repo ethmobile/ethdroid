a6af4934b8b208ad9db5455942aff2aea0904683
Nicolas Guillaume <gunicolas@sqli.com>
2017-05-22 11:50:13 +0200

    new sample + upgrade to gradle 4.0 + fix transaction.data() bug with okio.ByteString + fix contract instanciation issue + add method to send transaction to contract

---
+++ ethdroid-sample/src/main/java/com/sqli/blockchain/ethdroid_sample/ITestContract.java
@@ -0,0 +1,39 @@
+package com.sqli.blockchain.ethdroid_sample;
+
+import com.sqli.blockchain.ethdroid.solidity.ContractType;
+import com.sqli.blockchain.ethdroid.solidity.element.SolidityElement;
+import com.sqli.blockchain.ethdroid.solidity.element.event.SolidityEvent;
+import com.sqli.blockchain.ethdroid.solidity.element.event.SolidityEvent3;
+import com.sqli.blockchain.ethdroid.solidity.element.function.SolidityFunction;
+import com.sqli.blockchain.ethdroid.solidity.element.function.SolidityFunction2;
+import com.sqli.blockchain.ethdroid.solidity.element.function.SolidityFunction3;
+import com.sqli.blockchain.ethdroid.solidity.types.SArray;
+import com.sqli.blockchain.ethdroid.solidity.types.SBool;
+import com.sqli.blockchain.ethdroid.solidity.types.SInt;
+import com.sqli.blockchain.ethdroid.solidity.types.SUInt;
+
+/**
+ * Created by gunicolas on 08/03/17.
+ */
+
+interface ITestContract extends ContractType {
+
+    SolidityFunction<SBool> foo();
+    SolidityFunction<SUInt.SUInt8> value();
+    SolidityFunction<SUInt.SUInt8> bar(SUInt.SUInt8 a);
+
+    /*
+    pragma solidity ^0.4.10;
+    contract Simple {
+        uint8 public value;
+        function foo() returns(bool){
+            return true;
+        }
+        function bar(uint8 a) returns(uint8){
+            value = a;
+            return value;
+        }
+    }
+    */
+
+}
