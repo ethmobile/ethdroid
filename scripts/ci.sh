40cb55db6acbb4a614a97c795f89bff1511b1152
Nigui <gunicolas@sqli.com>
2017-06-06 09:34:14 +0200

    Travis (#26) : add log outputs to debug travis variables

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,9 +2,13 @@
 
 set -ev
 
+echo "IS PULL REQUEST : "$TRAVIS_PULL_REQUEST
+echo "IS BRANCH : "$TRAVIS_BRANCH
 
-if [[ $TRAVIS_PULL_REQUEST == “false” ]] && [[ $TRAVIS_BRANCH == “master” ]]; then
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     PUBLISH="artifactoryPublish"
 fi
+
+echo "PUBLISH : "$PUBLISH
 
 ./gradlew clean build connectedCheck -PdisablePreDex $PUBLISH --stacktrace --info
